23a9b5ba12d88c5fb26d18a2347a1a5c4f7821a3
aciukurescu <andrewciukurescu@gmail.com>
2018-01-18 15:50:55 -0500

    text added to modal

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var close = document.getElementById('close')
 
 setTimeout(function() {
 	modal.style.display = "block";
+	animation: "fade";
 }, 3000);
 
 // function xout(){
@@ -19,3 +20,4 @@ setTimeout(function() {
 close.addEventListener('click', function(){
 	modal.style.display = "none";
 })
+
